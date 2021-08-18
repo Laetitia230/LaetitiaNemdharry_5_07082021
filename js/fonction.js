d1e4ce675a37b32f841742483e552436c7659196
Laetitia Nemdharry <laetitia@pc-475.home>
2021-08-18 12:19:17 +0200

    Modif basic

--- js/fonction.js
+++ js/fonction.js
@@ -72,16 +72,17 @@ function totalPrice() {
 
 // calcul du basketPreview
 function basketPreview() {
+    console.log(basket);
     if (basket.length == 0) {
     } else {
         let addBasketPreview = document.getElementById("basketPreview");
         let calculBasketPreview = 0;
+        console.log(calculBasketPreview);
         for (product of basket) {
             calculBasketPreview += product.quantity;
+            
         }
         addBasketPreview.innerHTML += `Panier <span class="badge rounded-pill bg-secondary align-middle my-auto">${calculBasketPreview}</span>`;
-
-
     }
 }
 
